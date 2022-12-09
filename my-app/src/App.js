296982bde9ef084ac568bdf8c196d874869d25cd
Sheehan Maitra <sheehanmaitra@gmail.com>
2022-12-09 02:25:14 -0500

    intialize

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+//Components
+import NavBar from "./components/NavBar";
+//Main Style Sheet
+import "./App.css";
+function App() {
+  return (
+    <div className="app">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
